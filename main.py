e00e555facad1800c87dd83cc9e69011c2a25d20
matehrani <t.mohammadamin@gmail.com>
2023-07-07 08:54:24 +0200

    change main,.gitignore

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 from data_manager import DBManager
 from core.models import User
 
-manager = DBManager({'db_config':{
-    'dbname':'eemjflhg',
-    'host':'kesavan.db.elephantsql.com',
-    'password':'17CkxoiUV7eQGqXLxXZGKstCpgBCE0rM',
-    'user':'eemjflhg'
-}})
+db_config = {
+    # TODO
+}
 
-
-print("All users:", *manager.read_all(User), sep='\n')
-
-
-
+manager = DBManager({'db_config':db_config})
