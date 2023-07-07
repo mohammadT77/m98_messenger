bcf76dbd3c0735ec1339d4259369fbe79aaea37b
matehrani <t.mohammadamin@gmail.com>
2023-07-07 07:59:26 +0200

    impl: db_manager.truncate

--- data_manager/db_manager.py
+++ data_manager/db_manager.py
@@ -116,8 +116,12 @@ class DBManager(BaseManager):
 
 
     def truncate(self, model_cls: type) -> None:
-        #TODO: Complete
-        pass
+        assert issubclass(model_cls, BaseModel)
+        assert getattr(model_cls, 'TABLE_NAME', None), "Could not find TABLE NAME"
+
+        with self.__conn.cursor() as curs:
+            curs.execute(f"TRUNCATE TABLE {model_cls.TABLE_NAME}")
+        self.__conn.commit()
 
     
     
